2989db0d05b62e94386d01b1b1b832e0f9fc98fe
penvarict <penvarict@s.dcsdk12.org>
2017-10-06 11:16:51 -0600

    finish testing

--- number_summary.rb
+++ number_summary.rb
@@ -13,21 +13,21 @@ end
 def median(array)
     items = 0
     medianNumber = 0.0
-    midIndex = 0
+    midIndex = items/2.0
     array.each do |number, i|
         items = items + 1 
-        midIndex = items / 2
+     
     end
     
     if items % 2 == 0 #if total of things is even
-        medianNumber = (array[midIndex-1] + array[midIndex + 1])/2.0  #array starts at 0, add one for items spot
+        medianNumber = (array[midIndex-1] + array[midIndex+ 1])/2.0  #array starts at 0, add one for items spot
     else
-        medianNumber = array[midIndex]
+        medianNumber = array[items/2].to_f
             
       
     end
     
-    return medianNumber.to_f
+    return medianNumber
 
     #items = 7 mindIndex = 3.5(4)
 
@@ -39,18 +39,44 @@ end
 def standard_deviation(array) #find mean, subtract mean and square result, average of squared diff
     mean_num = mean(array)
     squaredDiff = []
+    sqMean = mean(squaredDiff)
 
     array.each_with_index do |number,i|
-        squaredDiff.push((mean_num-number[i])*(mean_num-number[i]))
-
+        squaredDiff.push((number[i]-mean_num)*(number[i]-mean_num))
 
     end
 
-    return mean(squaredDiff)
+    return sqMean
 
 
 end
 
 
+def mode(array)
 
-print standard_deviation([6,2,3,1])
+    modeNumbers = []
+    countNumbers = 0
+    countNumbersMax = 0
+
+    array.each_with_index do |number, i|
+    
+        if array[i] = array[i+1]
+
+           modeNumbers.push(number[i])
+           countNumbers += 1
+
+
+        elsif array[i] = array[i+1] && countNumbers > countNumbersMax
+            modeNumbers.push(number[i])
+
+        end
+
+
+    end
+
+    return modeNumbers
+
+
+
+end
+
--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ describe 'number_summary' do
         it 'print out the mean of this array' do
             mean([2,3,3,4]).must_equal(3)
             mean([1,1,1,1]).must_equal(1)
-            mean ([21,2,23,4,42,0]).must_equal(15.333)
+            mean([21,2,23,4,42,0]).must_equal(15.333)
         end
     end
 
@@ -25,7 +25,7 @@ describe 'number_summary' do
 
     describe 'standard_deviation' do 
         it 'print out the standard deviation in a dataset' do
-            standard_deviation([1,1,1,1]).must_equal(0)
+           # standard_deviation([6,2,3,1]).must_equal(1.87)
             standard_deviation([3,4,7,20,21]).must_equal(8.8)
 
         end
@@ -33,5 +33,21 @@ describe 'number_summary' do
 
 
     end
+
+
+    describe 'mode' do
+
+        it 'print out a list of the mode' do
+            mode([1,2,2,2,4,5]).must_equal([2,2,2])
+            mode([1,2,3,4,5]).must_equal([1,2,3,4,5])
+            mode([1,1,2,2,3]).must_equal([1,1,2,2,3])
+
+        end
+
+
+    end
+
+
+
 
 end
